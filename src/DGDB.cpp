a579c937a50370ca98c20d299046dbffa6ff340f
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-06-19 15:18:30 -0500

    Fixed styles, deleted unused files, updated CMakeLists.txt

--- src/DGDB.cpp
+++ src/DGDB.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-void DGDB::runConnection(int Pconnection)
-{
+void DGDB::runConnection(int Pconnection) {
   int n;
   int s, ss;
   char buffer[1024];
@@ -15,46 +14,44 @@ void DGDB::runConnection(int Pconnection)
   bool existeAtributos = false;
 
   cout << "wwww" << endl;
-  while (server || repository)
-  {
+
+  while (server || repository) {
     n = read(Pconnection, buffer, 1);
+
     if (n < 0)
       perror("ERROR reading from socket");
-    else if (n == 0)
-    {
+    else if (n == 0) {
     }
-    else if (n > 0)
-    {
+    else if (n > 0) {
       printf("%s\n", buffer);
     }
-    if (buffer[0] == 'C')
-    {
+
+    if (buffer[0] == 'C') {
       cout << "Action:C\n";
       n = read(Pconnection, buffer, 3);
-      if (n < 3)
-      {
+
+      if (n < 3) {
         perror("ERROR reading size\n");
       }
-      else
-      {
+      else {
         buffer[n + 1] = '\0';
         s = atoi(buffer) + 2;
       }
+
       n = read(Pconnection, buffer, s);
-      if (n < s)
-      {
+
+      if (n < s) {
         perror("ERROR reading size\n");
       }
-      else
-      {
+      else {
         int r;
-        if (buffer[s - 1] == '0') // no existen relaciones
-        {
+
+        if (buffer[s - 1] == '0') { // no existen relaciones
           cout << "No se envio relaciones" << endl;
           buffer[s - 1] = '\0';
         }
-        else if (buffer[s - 1] - '0' > 0)
-        { // exite almenos una relacion
+        else if (buffer[s - 1] - '0' > 0) {
+          // exite almenos una relacion
           existeRelaciones = true;
           n = read(Pconnection, bufferB, 3);
           bufferB[3] = '\0';
@@ -64,76 +61,72 @@ void DGDB::runConnection(int Pconnection)
 
           cout << "nodeB:" << bufferB << endl;
         }
-        if (buffer[s - 2] == '0') // no existen atributos
-        {
+
+        if (buffer[s - 2] == '0') { // no existen atributos
           cout << "No se envio atributos" << endl;
           buffer[s - 2] = '\0';
         }
+
         cout << "data:[" << buffer << "]" << endl;
         data = buffer;
-        if (socketRepositories.size() > 1)
-        {
+
+        if (socketRepositories.size() > 1) {
           // enviar al  Repository
           // determinar que repository
-          if (socketRepositories.size() < 2)
-          {
+          if (socketRepositories.size() < 2) {
             cout << "Repository has not been attached." << endl;
             return;
           }
           else
             r = buffer[0] % (socketRepositories.size() - 1);
+
           r++;
           cout << "xxxx--:" << socketRepositories.size() << endl;
           cout << "xxxx-r:" << r << endl;
           cout << "xxxx-s:" << socketRepositories[r] << endl;
           cout << "xxxx-d:" << data << endl;
-          if (existeRelaciones && !existeAtributos)
-          {
+
+          if (existeRelaciones && !existeAtributos) {
             createRelation(data, bufferB, socketRepositories[r]);
           }
-          else if (existeRelaciones && existeAtributos)
-          {
+          else if (existeRelaciones && existeAtributos) {
             //
           }
-          else if (!existeRelaciones && existeAtributos)
-          {
+          else if (!existeRelaciones && existeAtributos) {
             //
           }
-          else if (!existeRelaciones && !existeAtributos)
-          {
+          else if (!existeRelaciones && !existeAtributos) {
             createNode(data, socketRepositories[r]);
           }
 
           cout << "xxxx" << endl;
         }
-        else if (repository)
-        {
+        else if (repository) {
           cout << "Store:" << data << "-" << bufferB << endl;
         }
       }
     }
-    if (buffer[0] == 'R')
-    {
+
+    if (buffer[0] == 'R') {
     }
-    if (buffer[0] == 'U')
-    {
+
+    if (buffer[0] == 'U') {
     }
-    if (buffer[0] == 'D')
-    {
+
+    if (buffer[0] == 'D') {
     }
-    if (buffer[0] == 'R')
-    {
+
+    if (buffer[0] == 'R') {
       n = read(Pconnection, buffer, 21);
-      if (n < 21)
-      {
+
+      if (n < 21) {
         perror("ERROR reading size\n");
       }
-      else
-      {
+      else {
         char vIp[17];
         char vPort[6];
         int vPort_int;
-        char *pbuffer;
+        char* pbuffer;
         strncpy(vPort, buffer, 5);
         vPort_int = atoi(vPort);
         pbuffer = &buffer[5];
@@ -142,8 +135,8 @@ void DGDB::runConnection(int Pconnection)
         string vIp_string = vIp;
         trim(vIp_string);
         connMasterRepository(vPort_int, vIp_string);
-        if (repository)
-        {
+
+        if (repository) {
           socketRepositories.push_back(socketRepository);
           cout << "Repository registed." << endl;
         }
@@ -156,141 +149,144 @@ void DGDB::runConnection(int Pconnection)
     //if (n < 0) perror("ERROR writing to socket");
     /* perform read write operations ... */
   }
+
   connections.erase(Pconnection);
   shutdown(Pconnection, SHUT_RDWR);
   close(Pconnection);
 }
 
-void DGDB::runMainServer()
-{
-  for (;;)
-  {
+void DGDB::runMainServer() {
+  for (;;) {
     int newConnection = accept(socketServer, NULL, NULL);
-    if (0 > newConnection)
-    {
+
+    if (0 > newConnection) {
       perror("error accept failed");
       close(newConnection);
       exit(EXIT_FAILURE);
     }
+
     std::thread(&DGDB::runConnection, this, newConnection).detach();
   }
 }
 
-void DGDB::runServer()
-{
-  if (mode == 'S')
-  {
+void DGDB::runServer() {
+  if (mode == 'S') {
     std::thread runThread(&DGDB::runMainServer, this);
     runThread.join();
   }
-  else if (mode == 'R')
-  {
+  else if (mode == 'R') {
     cout << "RRRRR" << endl;
     std::thread runThread(&DGDB::runRepository, this);
     runThread.join();
   }
 }
-void DGDB::setClient()
-{
+
+void DGDB::setClient() {
   int Res;
   socketCliente = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (-1 == socketCliente)
-  {
+
+  if (-1 == socketCliente) {
     perror("cannot create socket");
     exit(EXIT_FAILURE);
   }
+
   //memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
 
   stSockAddr.sin_family = AF_INET;
   stSockAddr.sin_port = htons(port);
   Res = inet_pton(AF_INET, ip.c_str(), &stSockAddr.sin_addr);
-  if (0 > Res)
-  {
+
+  if (0 > Res) {
     perror("error: first parameter is not a valid address family");
     close(socketCliente);
     exit(EXIT_FAILURE);
   }
-  else if (0 == Res)
-  {
+  else if (0 == Res) {
     perror("char string (second parameter does not contain valid ipaddress");
     close(socketCliente);
     exit(EXIT_FAILURE);
   }
-  Res == connect(socketCliente, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in));
-  if (Res == -1)
-  {
+
+  Res == connect(socketCliente, (const struct sockaddr*)&stSockAddr,
+                 sizeof(struct sockaddr_in));
+
+  if (Res == -1) {
     perror("connect failed");
     close(socketCliente);
     exit(EXIT_FAILURE);
   }
+
   connection = 1;
 }
-void DGDB::closeClient()
-{
+
+void DGDB::closeClient() {
   connection = 0;
   shutdown(socketCliente, SHUT_RDWR);
   close(socketCliente);
 }
 
-void DGDB::setServer()
-{
+void DGDB::setServer() {
   int Res;
   socketServer = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (-1 == socketServer)
-  {
+
+  if (-1 == socketServer) {
     perror("can not create socket");
     exit(EXIT_FAILURE);
   }
+
   stSockAddr.sin_family = AF_INET;
   stSockAddr.sin_port = htons(port);
   stSockAddr.sin_addr.s_addr = INADDR_ANY;
-  Res = bind(socketServer, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in));
-  if (-1 == Res)
-  {
+  Res = bind(socketServer, (const struct sockaddr*)&stSockAddr,
+             sizeof(struct sockaddr_in));
+
+  if (-1 == Res) {
     perror("error bind failed");
     close(socketServer);
     exit(EXIT_FAILURE);
   }
+
   Res = listen(socketServer, 10);
-  if (-1 == Res)
-  {
+
+  if (-1 == Res) {
     perror("error listen failed");
     close(socketServer);
     exit(EXIT_FAILURE);
   }
+
   server = 1;
 }
 
-void DGDB::closeServer()
-{
+void DGDB::closeServer() {
   server = 0;
   close(socketServer);
 }
 
-void DGDB::setRepository()
-{
-
+void DGDB::setRepository() {
   cout << "setRepository" << endl;
   int Res;
   socketRepository = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (-1 == socketRepository)
-  {
+
+  if (-1 == socketRepository) {
     perror("can not create socket");
     exit(EXIT_FAILURE);
   }
+
   stSockAddr.sin_family = AF_INET;
   stSockAddr.sin_port = htons(port);
   stSockAddr.sin_addr.s_addr = INADDR_ANY;
-  Res = bind(socketRepository, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in));
-  if (-1 == Res)
-  {
+  Res = bind(socketRepository, (const struct sockaddr*)&stSockAddr,
+             sizeof(struct sockaddr_in));
+
+  if (-1 == Res) {
     perror("error bind failed");
     close(socketServer);
     exit(EXIT_FAILURE);
   }
+
   Res = listen(socketRepository, 10);
-  if (-1 == Res)
-  {
+
+  if (-1 == Res) {
     perror("error listen failed");
     close(socketRepository);
     exit(EXIT_FAILURE);
@@ -301,32 +297,30 @@ void DGDB::setRepository()
   mode = 'R';
 }
 
-void DGDB::setNode(string name)
-{
+void DGDB::setNode(string name) {
   createNode(name, socketCliente);
 }
 
-void DGDB::setRelation(string nameA, string nameB)
-{
+void DGDB::setRelation(string nameA, string nameB) {
   createRelation(nameA, nameB, socketCliente);
 }
-void DGDB::createRelation(string nameA, string nameB, int conn ){
 
+void DGDB::createRelation(string nameA, string nameB, int conn ) {
   //string tmp = nameA + "-" + nameB;
   //int n = write(socketCliente,tmp.c_str(),tmp.length());
 
   /*
-int action; // 1B CRUD                  C
-int name_node_size;// 3B                3
-char name_node[255];// VB               15151515 (DNI)
-int number_of_attributes; // 2B         0
-int number_of_relations;  // 3B         1
+  int action; // 1B CRUD                  C
+  int name_node_size;// 3B                3
+  char name_node[255];// VB               15151515 (DNI)
+  int number_of_attributes; // 2B         0
+  int number_of_relations;  // 3B         1
 
-// relations are optional
-int node_relation_size;  // 3B
-char node_relations[255];// VB
+  // relations are optional
+  int node_relation_size;  // 3B
+  char node_relations[255];// VB
 
-*/
+  */
 
   char tamano[4];
   sprintf(tamano, "%03d", nameA.length());
@@ -342,14 +336,13 @@ char node_relations[255];// VB
   int n;
   //cout << "*" << buffer.c_str() << "*" << "endl";
   n = write(conn, buffer.c_str(), buffer.length());
-  if (n < 0)
-  {
+
+  if (n < 0) {
     perror("error listen failed");
     close(conn);
     exit(EXIT_FAILURE);
   }
-  else if (n > 0 && n != buffer.length())
-  {
+  else if (n > 0 && n != buffer.length()) {
     /* code */
     perror("error listen failed\n");
   }
@@ -357,16 +350,14 @@ char node_relations[255];// VB
 
 /// Protocolo
 
-void DGDB::createNode(string name, int conn)
-{
-
+void DGDB::createNode(string name, int conn) {
   /*
-int action; // 1B CRUD                  C
-int name_node_size;// 3B                3
-char name_node[255];// VB               15151515 (DNI)
-int number_of_attributes; // 2B         1
-int number_of_relations;  // 3B         0
-*/
+  int action; // 1B CRUD                  C
+  int name_node_size;// 3B                3
+  char name_node[255];// VB               15151515 (DNI)
+  int number_of_attributes; // 2B         1
+  int number_of_relations;  // 3B         0
+  */
 
   char tamano[4];
   sprintf(tamano, "%03d", name.length());
@@ -376,26 +367,23 @@ int number_of_relations;  // 3B         0
   int n;
   //cout << "*" << buffer.c_str() << "*" << "endl";
   n = write(conn, buffer.c_str(), buffer.length());
-  if (n < 0)
-  {
+
+  if (n < 0) {
     perror("error listen failed");
     close(socketCliente);
     exit(EXIT_FAILURE);
   }
-  else if (n > 0 && n != buffer.length())
-  {
+  else if (n > 0 && n != buffer.length()) {
     /* code */
   }
 }
 
-void DGDB::registerRepository()
-{
-
+void DGDB::registerRepository() {
   /*
-int action; // X
-int port; // 5B
-char ip[16] VB
-*/
+  int action; // X
+  int port; // 5B
+  char ip[16] VB
+  */
   cout << ip << endl;
   string buffer;
   char vip[17];
@@ -411,101 +399,106 @@ char ip[16] VB
   // Set conn to Main
   int Res;
   int socketMain = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (-1 == socketMain)
-  {
+
+  if (-1 == socketMain) {
     cout << "cannot create socket" << endl;
     exit(EXIT_FAILURE);
   }
+
   //memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
   cout << "*1*" << endl;
   stSockAddr.sin_family = AF_INET;
   stSockAddr.sin_port = htons(mainPort);
   Res = inet_pton(AF_INET, mainIp.c_str(), &stSockAddr.sin_addr);
-  if (0 > Res)
-  {
+
+  if (0 > Res) {
     cout << "error: first parameter is not a valid address family" << endl;
     close(socketMain);
     exit(EXIT_FAILURE);
   }
-  else if (0 == Res)
-  {
-    cout << "char string (second parameter does not contain valid ipaddress" << endl;
+  else if (0 == Res) {
+    cout << "char string (second parameter does not contain valid ipaddress" <<
+         endl;
     close(socketMain);
     exit(EXIT_FAILURE);
   }
+
   cout << "*2*" << endl;
-  Res == connect(socketMain, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in));
-  if (Res == -1)
-  {
+  Res == connect(socketMain, (const struct sockaddr*)&stSockAddr,
+                 sizeof(struct sockaddr_in));
+
+  if (Res == -1) {
     cout << "connect failed" << endl;
     close(socketMain);
     exit(EXIT_FAILURE);
   }
+
   //
   cout << "*3*" << endl;
   int n = write(socketMain, buffer.c_str(), buffer.length());
   cout << "*4*" << endl;
-  if (n < 0)
-  {
+
+  if (n < 0) {
     cout << "error listen failed" << endl;
     close(socketMain);
     exit(EXIT_FAILURE);
   }
-  else if (n > 0 && n != buffer.length())
-  {
+  else if (n > 0 && n != buffer.length()) {
     cout << "Registing Repository:[" << buffer << "]" << endl;
   }
+
   cout << n << endl;
 }
 
-void DGDB::runRepository()
-{
-
-  for (;;)
-  {
+void DGDB::runRepository() {
+  for (;;) {
     int newConnection = accept(socketRepository, NULL, NULL);
-    if (0 > newConnection)
-    {
+
+    if (0 > newConnection) {
       perror("error accept failed");
       close(newConnection);
       exit(EXIT_FAILURE);
     }
+
     std::thread(&DGDB::runConnection, this, newConnection).detach();
   }
 }
-void DGDB::connMasterRepository(int pPort, string pIp)
-{
+
+void DGDB::connMasterRepository(int pPort, string pIp) {
   cout << pPort << "-" << pIp << endl;
   int Res;
   socketRepository = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (-1 == socketRepository)
-  {
+
+  if (-1 == socketRepository) {
     perror("cannot create socket");
     exit(EXIT_FAILURE);
   }
+
   //memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
 
   stSockAddr.sin_family = AF_INET;
   stSockAddr.sin_port = htons(pPort);
   Res = inet_pton(AF_INET, pIp.c_str(), &stSockAddr.sin_addr);
-  if (0 > Res)
-  {
+
+  if (0 > Res) {
     perror("error: first parameter is not a valid address family");
     close(socketRepository);
     exit(EXIT_FAILURE);
   }
-  else if (0 == Res)
-  {
+  else if (0 == Res) {
     perror("char string (second parameter does not contain valid ipaddress");
     close(socketRepository);
     exit(EXIT_FAILURE);
   }
-  Res == connect(socketRepository, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in));
-  if (Res == -1)
-  {
+
+  Res == connect(socketRepository, (const struct sockaddr*)&stSockAddr,
+                 sizeof(struct sockaddr_in));
+
+  if (Res == -1) {
     perror("connect failed");
     close(socketRepository);
     exit(EXIT_FAILURE);
   }
+
   repository = 1;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,31 @@
 #include "DGDB.h"
 
-
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    DGDB db;
-    if (argv[1][0] == 'C'){
-        db.setMode('C');
-        db.setClient();
-        //db.setNode(argv[2]);
-        db.setRelation(argv[2],argv[3]);
-    }
-    else if (argv[1][0] == 'S'){
-        db.setMode('S');
-        db.setServer();
-        db.runServer();
-    }
-    else if (argv[1][0] == 'R'){
-        db.setPort(atoi(argv[2]));
-        db.setIp("127.0.0.1");
-        db.setMainIp("127.0.0.1");
-        db.setMainPort(50000);
-        db.setMode('R');
-        db.setRepository();
-        db.runServer();
+int main(int argc, char* argv[]) {
+  DGDB db;
 
-    }
+  if (argv[1][0] == 'C') {
+    db.setMode('C');
+    db.setClient();
+    //db.setNode(argv[2]);
+    db.setRelation(argv[2], argv[3]);
+  }
+  else if (argv[1][0] == 'S') {
+    db.setMode('S');
+    db.setServer();
+    db.runServer();
+  }
+  else if (argv[1][0] == 'R') {
+    db.setPort(atoi(argv[2]));
+    db.setIp("127.0.0.1");
+    db.setMainIp("127.0.0.1");
+    db.setMainPort(50000);
+    db.setMode('R');
+    db.setRepository();
+    db.runServer();
+
+  }
 
 }
 
@@ -39,7 +37,7 @@ char name_node[255];// VB               15151515 (DNI)
 int number_of_attributes; // 2B         1
 int number_of_relations;  // 3B         0
 
-// attributes are optional  
+// attributes are optional
 int name_attribute_size;  //  3B        13
 char name_attribute[255]; //  VB        Primer Nombre
 int value_attribute_size; //  3B        5
@@ -54,24 +52,24 @@ tabla de Nodos
 Julio
 Fabiola
 
-tabla de Relaciones 
-Julio  |  Fabiola 
+tabla de Relaciones
+Julio  |  Fabiola
 
 tabla atributos
 Julio  | edad | 48
-Julio  | grad Aca | Phd 
+Julio  | grad Aca | Phd
 Julio  | Est. Civ. | Casado
 
 
 
-9B  VB ?A  6B VB ?R 3B VB 
+9B  VB ?A  6B VB ?R 3B VB
 int action; // 1B CRUD                  C
 int name_node_size;// 3B                3
 int number_of_attributes; // 2B         1
 int number_of_relations;  // 3B         0
 char name_node[255];// VB               15151515 (DNI)
 
-// attributes are optional  
+// attributes are optional
 int name_attribute_size;  //  3B        13
 int value_attribute_size; //  3B        5
 char name_attribute[255]; //  VB        Primer Nombre
@@ -83,11 +81,11 @@ char node_relations[255];// VB
 
 ////////////////////////////////////////////
 
-int action; //       1B CRUD                  R 
+int action; //       1B CRUD                  R
 int query_node_size ; //  2B                  0
 char query_node[99];  //  VB                  0
 int  deep; //        1B                       3
-int  leaf; // on/off 1B                       off  
+int  leaf; // on/off 1B                       off
 int attributes; // on/off 1B                  1       //  se quiere extraer los atributos
 
 int number_of_conditions;  // 2B              3
@@ -126,7 +124,7 @@ char set_value_node[99];  //  VB                        Omar
 
 int query_attribute_size;  //          3B        15
 int query_value_attribute_size; //  3B           Grado academico
-int set_attribute_size;  //  3B        13        2 
+int set_attribute_size;  //  3B        13        2
 int set_value_attribute_size; //  3B             Dr
 
 ////////////////////////////////////////////
