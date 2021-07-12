f4b5f58b7d5c93fb207574941672cdf4118265d8
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-07-11 19:18:41 -0500

    Added constructor to RDT_UDP

--- src/rdt/RDT.h
+++ src/rdt/RDT.h
@@ -9,12 +9,14 @@
 
 class RDT_UDP {
  public:
+  explicit RDT_UDP(size_t max_packet_length=1000)
+    : rdt_udp_helper(max_packet_length) {}
   int SendTo(const std::string& ip_addr, uint16_t port, const char* buffer,
              int len, int flags=0);
   int RecvFrom(char* buffer, int len, int flags=0);
 
  private:
-  Network::UDPSocket server;
+  Network::UDPSocket udp_socket;
   RDT_UDP_Helper rdt_udp_helper;
 };
 
